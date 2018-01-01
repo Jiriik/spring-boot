775a8b95b4b517e8c09f576ebb174f2379c4ebbd
jiriik <vlasimsky.jiri@gmail.com>
2018-01-01 17:11:07 +0100

    Initial Commit

---
+++ todoMicroservice/src/main/java/cz/vlasimsky/springboot/todoMicroservice/TodoMicroserviceApplication.java
@@ -0,0 +1,12 @@
+package cz.vlasimsky.springboot.todoMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoMicroserviceApplication.class, args);
+	}
+}
