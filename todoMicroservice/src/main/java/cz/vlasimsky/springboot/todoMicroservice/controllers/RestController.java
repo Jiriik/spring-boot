981e56ce775208d94a457a5cbc1a5b521b6a8244
jiriik <vlasimsky.jiri@gmail.com>
2018-01-02 14:40:18 +0100

    JSR and Spring validator for RestController

--- todoMicroservice/src/main/java/cz/vlasimsky/springboot/todoMicroservice/controllers/RestController.java
+++ todoMicroservice/src/main/java/cz/vlasimsky/springboot/todoMicroservice/controllers/RestController.java
@@ -2,7 +2,11 @@ package cz.vlasimsky.springboot.todoMicroservice.controllers;
 
 import cz.vlasimsky.springboot.todoMicroservice.entities.Todo;
 import cz.vlasimsky.springboot.todoMicroservice.entities.User;
+import cz.vlasimsky.springboot.todoMicroservice.utilities.ToDoValidator;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
@@ -22,4 +26,29 @@ public class RestController {
     public String todoInInput1(Todo todo) {
         return "Todo description = " + todo.getDescription() + ", priority = " + todo.getPriority();
     }
+
+    @RequestMapping("/todoInInput2")
+    public String todoInInput2(@Valid Todo todo) {
+        return "Todo description = " + todo.getDescription() + ", priority = " + todo.getPriority();
+    }
+
+    @RequestMapping("/todoInInput3")
+    public String todoInInput3(@Valid Todo todo, BindingResult result) {
+        if (result.hasErrors()) {
+            return "my custom formatted error messages";
+        }
+        return "Todo description = " + todo.getDescription() + ", priority = " + todo.getPriority();
+    }
+
+    @RequestMapping("/todoInInput4")
+    public String todoInInput4( Todo todo, BindingResult result) {
+        ToDoValidator validator = new ToDoValidator();
+        validator.validate(todo, result);
+        if (result.hasErrors()) {
+            return "my custom formatted error messages" + result.toString();
+        }
+        return "Todo description = " + todo.getDescription() + ", priority = " + todo.getPriority();
+    }
+
+
 }
