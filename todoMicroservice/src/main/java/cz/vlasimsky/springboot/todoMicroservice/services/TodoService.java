c991c0bddf6ecc0497cfe1f43a627a2924f37e2d
jiriik <vlasimsky.jiri@gmail.com>
2018-01-02 15:36:40 +0100

    Services preparation for todos

--- todoMicroservice/src/main/java/cz/vlasimsky/springboot/todoMicroservice/services/TodoService.java
+++ todoMicroservice/src/main/java/cz/vlasimsky/springboot/todoMicroservice/services/TodoService.java
@@ -1,4 +1,13 @@
 package cz.vlasimsky.springboot.todoMicroservice.services;
 
+import cz.vlasimsky.springboot.todoMicroservice.entities.Todo;
+
+import java.util.List;
+
 public interface TodoService {
+    List<Todo> getTodos(String email);
+
+    Todo addTodo(Todo todo);
+
+    void deleteTodo(Integer id);
 }
--- todoMicroservice/src/main/java/cz/vlasimsky/springboot/todoMicroservice/services/TodoServiceImpl.java
+++ todoMicroservice/src/main/java/cz/vlasimsky/springboot/todoMicroservice/services/TodoServiceImpl.java
@@ -1,5 +1,28 @@
 package cz.vlasimsky.springboot.todoMicroservice.services;
 
+import cz.vlasimsky.springboot.todoMicroservice.dao.TodoDao;
+import cz.vlasimsky.springboot.todoMicroservice.entities.Todo;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
 public class TodoServiceImpl implements TodoService {
 
+    @Autowired
+    TodoDao todoDao;
+
+    @Override
+    public List<Todo> getTodos(String email) {
+        return todoDao.findByFkUser(email);
+    }
+
+    @Override
+    public Todo addTodo(Todo todo) {
+        return todoDao.save(todo);
+    }
+
+    @Override
+    public void deleteTodo(Integer id) {
+        todoDao.delete(id);
+    }
 }
